27935a8c2c4433b3407b73e521eb42b986be56d9
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-08 15:48:32 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
- 
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import catsReducer from './reducers/catsReducer.js';
- 
+
 const store = createStore(catsReducer)
- 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
